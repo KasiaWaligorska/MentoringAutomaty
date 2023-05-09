0b242b9629eeda05484e771152c9678b23dc8a83
Kejtbusz <iwonaantonina90@gmail.com>
2023-05-09 12:44:07 +0200

    Initial commit

--- src/main/java/ZadanieDziedziczenie/Main.java
+++ src/main/java/ZadanieDziedziczenie/Main.java
@@ -1,2 +1,30 @@
-package ZadanieDziedziczenie;public class Main {
+package ZadanieDziedziczenie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pizzeria amoreMio = new Pizzeria(0, "amoreMio", "Mario", "Luigi");
+/*        amoreMio.sellMeals("Pizza");
+        amoreMio.sellMeals("Pizza");
+        amoreMio.getCashAmount();*/
+
+        FastFood burgerKing = new FastFood(0, "Burger King", "John", "Janusz Kowalski");
+/*        burgerKing.sellMeals("Burger");
+        burgerKing.sellMeals("French fries");
+        burgerKing.getCashAmount();
+        burgerKing.getListWithOrderedMeals();*/
+
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(amoreMio);
+        restaurants.add(burgerKing);
+
+        Customer customer = new Customer("Kasia", 1000000);
+        customer.buyMealFromPizzeria("Pizza", amoreMio);
+        customer.buyMealFastFood("Burger", burgerKing);
+        customer.getCash();
+        customer.getOrderedList();
+    }
 }
--- src/main/java/ZadanieDziedziczenie/Restaurant.java
+++ src/main/java/ZadanieDziedziczenie/Restaurant.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Restaurant {
+package ZadanieDziedziczenie;
+
+public abstract class Restaurant {
+
+    public int cashAmount;
+    public String name;
+    public String owner;
+
+    public Restaurant(int cashAmount, String name, String owner){
+        this.cashAmount = cashAmount;
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public void sellMeals(){
+        System.out.println("Ta metoda nic nie robi");
+    };
+
+
 }
